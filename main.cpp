55343c8cce5045c4e6ec83b904633fe48ee2eb1e
rockmanvnx6 <rockmanvnx6@gmail.com>
2020-10-05 00:43:06 +1100

    commenting for main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,29 @@
 
 using namespace std;
 
+/**
+ * File: main.cpp
+ * Author: s3619352@student.rmit.edu.au
+ * 
+ * Main file that allows to call allocator code (located in alloc.h).
+ * 
+ * Usage: main.cpp <strategy> <input_file> <output_file>
+ * Strategy List:
+ * - first_fit: perform first fit allocation on <input_file>
+ * - best_fit : perform best fit allocation on <input_file>
+ * - worst_fit: perform worst fit allocation on <input_file>
+ * 
+ * On success: result of the allocation will be saved in <output_file>
+ * which includes: total memory allocated using sbrk, start memory and size
+ * of each node in allocMB list and freeMB list.
+ * 
+ * On failure: 
+ * - return EINVAL (invalid argument) if the number of argument
+ * is not 4.
+ * - return ENOENT (no such file or directory) if <input_file> is not found.
+ * 
+ */
+
 void print_usage(string argv0) {
     cout << "USAGE: " << argv0 << " <strategy> <input_file> <output_file>" << endl;
     cout << endl;
@@ -20,12 +43,12 @@ void print_usage(string argv0) {
 }
 
 int main(int argc, char* argv[]) {
-    srand((unsigned int)time(NULL));
-    string line;
-
+    /* Setting up environment */
+    srand((unsigned int)time(NULL)); // Define randomness
     list<MemoryNode> alloc_mb;
     list<MemoryNode> free_mb;
 
+    /* Check valid usage */
     if (argc != 4) {
         print_usage(argv[0]);
         return EINVAL;
@@ -42,9 +65,11 @@ int main(int argc, char* argv[]) {
         return EINVAL;
     }
 
+    /* Allocate line by line from <input_file>*/
     Allocator *allocator = new Allocator(&alloc_mb, &free_mb, strategy); 
 
     int count = 0;
+    string line;
     if (input_file.is_open()) {
         while (getline(input_file, line)) {
             string name;
@@ -52,6 +77,7 @@ int main(int argc, char* argv[]) {
             allocator->alloc(name);
             count++;
             if (count > 1000) {
+                /* If exceeds 1000, remove 500 (as the specs) */
                 int to_remove = 500;
                 while(to_remove > 0) {
                     allocator->random_dealloc();
@@ -67,6 +93,7 @@ int main(int argc, char* argv[]) {
 
     input_file.close();
 
+    /* Write output into <output_file> */
     output_file.open(argv[3]);
     output_file << "Total SBRK allocation: " << allocator->total_sbrk_alloc << endl;
     list<MemoryNode>::iterator iter_alloc = alloc_mb.begin();
@@ -78,12 +105,14 @@ int main(int argc, char* argv[]) {
         iter_alloc++;
     }
     output_file << endl;
+
     output_file << "------FREE LIST------" << endl;
     while (iter_free != free_mb.end()) {
         output_file<< iter_free->start_memory << "\t" << iter_free->size << endl;
         iter_free++;
     }
 
+    /* Complete. Finish the script */
     cout << "Completed. Please check " << argv[3] << " for details." << endl;
     return 0;
 }
