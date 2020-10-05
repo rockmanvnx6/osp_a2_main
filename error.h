5ab22197071cb14f96dbc12178d0b2c3c9982ece
rockmanvnx6 <rockmanvnx6@gmail.com>
2020-10-05 14:22:02 +1100

    typo

--- error.h
+++ error.h
@@ -13,7 +13,7 @@ namespace ERROR {
      * Reference: https://linux.die.net/man/2/sbrk
      */
     const string FILE_NOT_FOUND   = "Input file not found.";
-    const string MEMORY_ERROR     = "MEMORRY ERROR - Insufficient memory.";
+    const string MEMORY_ERROR     = "MEMORY ERROR - Insufficient memory.";
     const string INVALID_STRAT    = "Invalid name of strategy.";
     void  *SBRK_MEMORY_ERROR      = (void *) -1;
 }
